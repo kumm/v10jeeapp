9fa2948043535b8b57a9e116de413a42850f003a
kumm <kumm@webstar.hu>
2018-05-27 13:40:56 +0200

    transform LoginVew to HasErrorParameter<UnauthenticatedException>

--- src/main/java/com/acme/v10jeeapp/app/security/NavigationInterceptor.java
+++ src/main/java/com/acme/v10jeeapp/app/security/NavigationInterceptor.java
@@ -1,28 +1,21 @@
 package com.acme.v10jeeapp.app.security;
 
-import com.acme.v10jeeapp.app.ui.LoginView;
 import com.vaadin.flow.router.BeforeLeaveEvent;
-import com.wcs.vaadin.flow.cdi.RouteScopeOwner;
 import org.apache.shiro.aop.AnnotationResolver;
 import org.apache.shiro.aop.MethodInvocation;
-import org.apache.shiro.authz.UnauthenticatedException;
+import org.apache.shiro.authz.AuthorizationException;
 import org.apache.shiro.authz.aop.AnnotationsAuthorizingMethodInterceptor;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.enterprise.inject.Typed;
-import javax.inject.Inject;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 
 @ApplicationScoped
 @Typed
 public class NavigationInterceptor extends AnnotationsAuthorizingMethodInterceptor {
-
-    @Inject
-    @RouteScopeOwner(LoginView.class)
-    private LoginView.AfterLoginNavigation afterLoginNavigation;
 
     @PostConstruct
     private void init() {
@@ -32,15 +25,11 @@ public class NavigationInterceptor extends AnnotationsAuthorizingMethodIntercept
                 -> interceptor.setResolver(annotationResolver));
     }
 
-    private void onBeforeLeave(@Observes BeforeLeaveEvent event) {
+    private void onBeforeLeave(@Observes BeforeLeaveEvent event)
+            throws AuthorizationException {
         NavigationTargetInvocation mi = new NavigationTargetInvocation(
                 event.getNavigationTarget());
-        try {
-            assertAuthorized(mi);
-        } catch (UnauthenticatedException e) {
-            afterLoginNavigation.setLocation(event.getLocation());
-            event.rerouteTo(LoginView.class);
-        }
+        assertAuthorized(mi);
     }
 
     private static class NavigationTargetInvocation implements MethodInvocation {
--- src/main/java/com/acme/v10jeeapp/app/ui/LoginView.java
+++ src/main/java/com/acme/v10jeeapp/app/ui/LoginView.java
@@ -6,47 +6,34 @@ import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.polymertemplate.EventHandler;
 import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.ErrorParameter;
+import com.vaadin.flow.router.HasErrorParameter;
 import com.vaadin.flow.router.Location;
-import com.vaadin.flow.router.Route;
 import com.vaadin.flow.templatemodel.TemplateModel;
-import com.vaadin.flow.theme.Theme;
-import com.vaadin.flow.theme.lumo.Lumo;
-import com.wcs.vaadin.flow.cdi.NormalRouteScoped;
-import com.wcs.vaadin.flow.cdi.RouteScopeOwner;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.authz.annotation.RequiresGuest;
+import org.apache.shiro.authz.UnauthenticatedException;
 
-import javax.inject.Inject;
-import java.io.Serializable;
+import javax.servlet.http.HttpServletResponse;
 
 @Tag("login-view")
 @HtmlImport("src/views/login/login-view.html")
-@Route("login")
-@Theme(Lumo.class)
-@RequiresGuest
-public class LoginView extends PolymerTemplate<LoginView.LoginModel>
-        implements HasLogger {
+// @Theme(Lumo.class) todo: breaks deployment, need to move to a common routerLayout
+public class LoginView
+        extends PolymerTemplate<LoginView.LoginModel>
+        implements HasErrorParameter<UnauthenticatedException>, HasLogger {
 
-    @Inject
-    @RouteScopeOwner(LoginView.class)
-    private AfterLoginNavigation afterLoginNavigation;
+    private Location targetLocation;
 
-    @NormalRouteScoped
-    @RouteScopeOwner(LoginView.class)
-    public static class AfterLoginNavigation implements Serializable {
-        private Location location = new Location("");
-
-        public void setLocation(Location location) {
-            this.location = location;
-        }
-
-        public void perform(UI ui) {
-            ui.navigate(location.getPath(), location.getQueryParameters());
-        }
+    @Override
+    public int setErrorParameter(BeforeEnterEvent event,
+                                 ErrorParameter<UnauthenticatedException> parameter) {
+        targetLocation = event.getLocation();
+        return HttpServletResponse.SC_OK;
     }
 
     @EventHandler
@@ -63,7 +50,11 @@ public class LoginView extends PolymerTemplate<LoginView.LoginModel>
             getLogger().error("Authentication error.", e);
             return;
         }
-        getUI().ifPresent(afterLoginNavigation::perform);
+        getUI().ifPresent(this::navigateToTarget);
+    }
+
+    private void navigateToTarget(UI ui) {
+        ui.navigate(targetLocation.getPath(), targetLocation.getQueryParameters());
     }
 
     public interface LoginModel extends TemplateModel {
