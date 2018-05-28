8b7d7a3b7c51c47b0b69610b1b11c4549a8cd501
kumm <kumm@webstar.hu>
2018-05-28 22:59:33 +0200

    Generate metamodel. Just in case.

--- src/main/java/com/acme/v10jeeapp/backend/EntityManagerProducer.java
+++ src/main/java/com/acme/v10jeeapp/backend/EntityManagerProducer.java
@@ -1,11 +1,29 @@
 package com.acme.v10jeeapp.backend;
 
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
 
+@ApplicationScoped
 public class EntityManagerProducer {
+
     @Produces
     @PersistenceContext
     private EntityManager em;
+
+    @Produces
+    @PersistenceUnit
+    private EntityManagerFactory emf;
+
+    @PostConstruct
+    private void init() {
+        // Workaround for persistence providers trying to
+        // load metamodel lazily but failing. ( like OpenJpa in TomEE )
+        emf.getMetamodel();
+    }
+
 }
